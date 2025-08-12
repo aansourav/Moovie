3279b1961e049e753d43dd6bfbfe04ed03ed1d8c
aansourav <aansourav@gmail.com>
2025-08-13 00:57:47 +0600

    Adds initial screen navigation and theme.
    Integrates `expo-router` to provide a link to the onboarding screen, establishing the initial user flow.
    Defines a comprehensive custom color palette in Tailwind CSS, foundational for consistent application theming and branding.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <View className="flex-1 items-center justify-center ">
       <Text className="text-red-600 text-5xl font-bold">Welcome to Moovie</Text>
+      <Link href="/onboarding" className="text-blue-500 text-2xl">
+        Onboarding
+      </Link>
     </View>
   );
 }
---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text>Onboarding Component</Text>
+    </View>
+  );
+};
+
+export default Onboarding;
