8a06d470295cdde04a8e6ca87ab566d213cabcbf
aansourav <aansourav@gmail.com>
2025-09-20 23:35:06 +0600

    Refactors main index screen layout
    Replaces the welcome text with a background image and introduces a ScrollView for future content. Updates the main view styling to enhance the overall appearance and user experience.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center ">
-      <Text className="text-red-600 text-5xl font-bold">Welcome to Moovie</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView>
+        
+      </ScrollView>
     </View>
   );
 }
